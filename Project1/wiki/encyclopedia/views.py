b3dcfd672ec3ead022b7935fe222ec2e5fbb75b6
MeDeveloperWeb <www.shivachaurasia@gmail.com>
2022-06-18 20:56:27 +0530

    First three steps done

--- Project1/wiki/encyclopedia/views.py
+++ Project1/wiki/encyclopedia/views.py
@@ -1,10 +1,37 @@
+from cgitb import html
+from django.http import HttpResponse
 from django.shortcuts import render
+from markdown2 import markdown_path
+from django import forms
 
 from . import util
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "head": "All Pages"
     })
 
+def entries(request, title):
+    html = markdown_path("entries/" + title + ".md")
+    return render(request, "encyclopedia/entries.html", {
+        "title" : title,
+        "html" : html
+    })
+
+def find(request):
+    query = request.GET.get("q").lower()
+
+    if query in [x.lower() for x in util.list_entries()]:
+        return entries(request, query)
+    
+    result = []
+    for every in util.list_entries():
+        if query in every.lower():
+            result.append(every)
+
+    return render(request, "encyclopedia/index.html", {
+        "entries": result,
+        "head": "Search Results"
+    })
